d2d83d4b59a635a1183186204cb959f30a247df4
nomfund <nomfundo.nkuta@umuzi.org>
2019-05-31 02:19:22 +0200

    added function for subtration

--- calculator.py
+++ calculator.py
@@ -1,13 +1,6 @@
 """
 Calculator library containing basic math operations.
 """
-
-def add(first_term, second_term):
-    return first_term + second_term
-
-def subtract(first_term, second_term):
-    return first_term - second_term
-
 def sum_numbers(*args):
     s = 0
     for i in args:
@@ -20,3 +13,6 @@ def multiply_numbers(*args):
     for i in args:
         s = s*i
     return s
+
+def subtract(first_term, second_term):
+    return first_term - second_term
--- calculator_test.py
+++ calculator_test.py
@@ -1,26 +1,24 @@
 """
 Unit tests for the calculator library
 """
-
 import calculator
-
 
 class TestCalculator:
 
     def test_zeros(self):
-        assert 0 == calculator.add(0,0)
+        assert 0 == calculator.sum_numbers(0,0)
 
     def test_negs(self):
-        assert -2 == calculator.add(-1,-1)
+        assert -2 == calculator.sum_numbers(-1,-1)
 
     def test_addition(self):
-        assert 9 == calculator.add(4, 5)
-
-    def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert 10 == calculator.sum_numbers(1,2,3,4)
 
     def test_sumation(self):
-        assert 3 == calculator.sum_numbers(1,1,1)
+        assert 3 == calculator.sum_numbers(1,2)
 
     def test_multiplication(self):
-        assert 4 == calculator.multiply_numbers(1,2,2)
+        assert 24 == calculator.multiply_numbers(1,2,3,4)
+        
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
